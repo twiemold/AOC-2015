aef653910bf8654840e672f444758ad36bba58b8
Thomas Wiemold <thomaswiemold@Thomass-MacBook-Air.local>
2023-10-29 10:37:46 -0400

    Simplify part one solution

--- day-one/src/day_one/core.clj
+++ day-one/src/day_one/core.clj
@@ -1,13 +1,21 @@
 (ns day-one.core
   (:gen-class))
 
-(defn count_braces [func braces nums]
+;; part one
+(defn count_braces [braces nums]
   (cond
     (empty? braces) nums
-    (= (first braces) \() (recur func (rest braces) (cons 1 nums))
-    (= (first braces) \)) (recur func (rest braces) (cons -1 nums))
+    (= (first braces) \() (recur (rest braces) (cons 1 nums))
+    (= (first braces) \)) (recur (rest braces) (cons -1 nums))
     ))
+
+;; part two
+;;(defn find_basement [func position floor]
+  ;;(case floor
+    ;;-1
 
 (defn -main
   [& args]
-  (println "The floor is: " (reduce + (count_braces count_braces (slurp "input.txt") '()))))
+  ;; part one
+  (println "The floor is: " (reduce + (count_braces (slurp "input.txt") '())))
+  )
