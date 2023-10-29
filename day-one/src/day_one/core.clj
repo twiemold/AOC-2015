6c8774920739d0a6fea90964c4e41856a4bf512c
Thomas Wiemold <thomas.wiemold@gmail.com>
2023-10-29 13:03:31 -0400

    Refactor

--- day-one/src/day_one/core.clj
+++ day-one/src/day_one/core.clj
@@ -3,20 +3,14 @@
 
 ;; part one
 (defn count_braces [braces nums]
-  (cond
-    (empty? braces) nums
-    (= (first braces) \() (recur (rest braces) (cons 1 nums))
-    (= (first braces) \)) (recur (rest braces) (cons -1 nums))
-    ))
+  (if (empty? braces) nums
+    (case (first braces)
+      \( (recur (rest braces) (cons 1 nums))
+      \) (recur (rest braces) (cons -1 nums)))))
 
 ;; part two
-(defn match_brace [brace]
-  (case brace
-    \( 1
-    \) -1))
-
 (defn find_basement [braces position floor]
-  (let [x (+ (match_brace (first braces)) floor)]
+  (let [x (+ (#(case % \( 1 \) -1) (first braces)) floor)]
   (case x
    ;; skipping base case since we know there is a solution
     -1 position
@@ -27,7 +21,7 @@
 (defn -main
   [& args]
   ;; part one
-  ;; (println "The floor is:" (reduce + (count_braces (slurp "input.txt") '())))
+  (println "The floor is:" (reduce + (count_braces (slurp "input.txt") '())))
   ;; part two
   (println "The position when he reaches the basement is:" (find_basement (slurp "input.txt") 1 0))
   )
