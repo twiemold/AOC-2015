5c201d2c8dd59ce9f4d577b33843e00527f34811
Thomas Wiemold <thomas.wiemold@gmail.com>
2023-11-06 17:16:13 -0500

    Refactor day two part one

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -3,12 +3,21 @@
     [clojure.string :as str])
   (:gen-class))
 
+(defn get-data 
+  [filename process-func]
+  (let [lines (line-seq (clojure.java.io/reader filename))]
+    (process-func lines)))
 
-(defn calc_surface_area [[l w h]] 
+(defn process-func 
+  [lines]
+  (into [] (map #(map read-string %) (map #(str/split %1 #"x") lines))))
+
+(defn calc_surface_area 
+  [[l w h]] 
   (let [sides [(* l w) (* w h) (* h l)]]
-        (+ (apply min sides) (reduce + (map (partial * 2) sides)))))
+    (+ (apply min sides) (reduce + (map (partial * 2) sides)))))
 
 
 (defn -main
   [& args]
-  (println "They should order this many sq ft:" (reduce + (doall (map calc_surface_area (partition 3 (map read-string (str/split (slurp "input.txt") #"x|\n"))))))))
+  (println "They should order this many sq ft:" (reduce + (map calc_surface_area (get-data "input.txt" process-func)))))
