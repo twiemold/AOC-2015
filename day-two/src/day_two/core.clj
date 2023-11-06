a4890570ca8f7ccdecba56f9e695e34a517da8a2
Thomas Wiemold <thomas.wiemold@gmail.com>
2023-11-06 18:52:53 -0500

    Solve day two part two

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -12,12 +12,21 @@
   [lines]
   (map #(map read-string %) (map #(str/split %1 #"x") lines)))
 
+;; part one
 (defn calc_surface_area 
   [[l w h]] 
   (let [sides [(* l w) (* w h) (* h l)]]
     (+ (apply min sides) (reduce + (map (partial * 2) sides)))))
 
+;; part two
+(defn calc-ribbon-length
+  [[l w h]]
+  (+ (reduce * [l w h]) (reduce + (map (partial * 2) (take 2 (sort [l w h]))))))
+
 
 (defn -main
   [& args]
-  (println "They should order this many sq ft:" (reduce + (map #(calc_surface_area %) (get-data "input.txt" process-func)))))
+  ;; part one
+  ;; (println "They should order this many sq ft:" (reduce + (map #(calc_surface_area %) (get-data "input.txt" process-func)))))
+  ;; part two
+  (println "They should order this many ft of ribbon:" (reduce + (map #(calc-ribbon-length %) (get-data "input.txt" process-func)))))
