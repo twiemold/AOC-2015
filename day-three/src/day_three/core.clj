06506a82e4c752741d66121f709af8f8732713d3
Thomas Wiemold <thomas.wiemold@gmail.com>
2023-11-10 22:37:25 -0500

    Revert change. Fix east-west

--- day-three/src/day_three/core.clj
+++ day-three/src/day_three/core.clj
@@ -10,18 +10,18 @@
     (let [[x y] curr]
       (case (first direction)
         \^ (recur (rest direction) (list x (+ y 1)) (conj coords (list x (+ y 1))))
-        \> (recur (rest direction) (list (+ x 1) y) (conj coords (list (+ x 1) y)))
-        \< (recur (rest direction) (list (- x 1) y) (conj coords (list (- x 1) y)))
+        \> (recur (rest direction) (list (- x 1) y) (conj coords (list (- x 1) y)))
+        \< (recur (rest direction) (list (+ x 1) y) (conj coords (list (+ x 1) y)))
         \v (recur (rest direction) (list x (- y 1)) (conj coords (list x (- y 1))))))))
 
 ;; part two
 (defn merge-coords [instructions]
-  (let [robo-santa (unique-coords (take-nth 2 instructions) '(0 0) (set '(0 0)))]
-    (let [santa (unique-coords (take-nth 2 (rest instructions)) '(0 0) (set '(0 0)))]
+  (let [robo-santa (unique-coords (take-nth 2 instructions) '(0 0) (set '()))]
+    (let [santa (unique-coords (take-nth 2 (rest instructions)) '(0 0) (set '()))]
       (set/union robo-santa santa))))
 
 (defn -main
   [& args]
   ;; part one
-  ;; (println "The count of houses visited at least once is:" (count (unique-coords (into [] (slurp "input.txt")) '(0 0) (set '(0 0)))))
+  (println "The count of houses visited at least once is:" (count (unique-coords (into [] (slurp "input.txt")) '(0 0) (set '()))))
   (println "The count of houses visited at least once is:" (count (merge-coords (into [] (slurp "input.txt"))))))
