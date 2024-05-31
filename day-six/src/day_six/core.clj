5b14de0e6c2c5329b00f867d2be91e9880e05a21
Thomas Wiemold <thomas.wiemold@gmail.com>
2024-05-31 10:21:53 -0400

    Solve day six part two.

--- day-six/src/day_six/core.clj
+++ day-six/src/day_six/core.clj
@@ -1,5 +1,20 @@
 (ns day-six.core)
 
+(defn switchV2 [grid command interval]
+  (let [op (case command
+             "turn on" #(inc %)
+             "turn off" #(if (> % 1) (dec %) 0)
+             "toggle" #(+ 2 %))] 
+   (loop [g grid 
+          coords (for [x (range (dec (get-in interval [0 0])) (get-in interval [1 0])) 
+                       y (range (dec (get-in interval [0 1])) (get-in interval [1 1]))] 
+                   (seq [(inc x) (inc y)]))] 
+     (if (empty? coords) 
+       g 
+       (recur (update-in g [(first (first coords)) 
+                            (second (first coords))] op) 
+              (rest coords))))))
+
 (defn switch [grid command interval]
   (let [op (case command
              "turn on" #(if (= % 0) 1 1)
@@ -22,19 +37,20 @@
         intervals (map vec (partition 2 (map vec (partition 2 (map parse-long (re-seq #"\d+" f))))))
         commands (re-seq #"turn\soff|turn\son|toggle" f)]
     (println 
-      "The number of lights lit is:" 
+      "The total brightness is:" 
              (reduce + (flatten 
                          (loop [g grid
                                 i intervals
                                 c commands]
                            (if (empty? c)
                              g
-                             (recur (switch g (first c) (first i)) (rest i) (rest c)))))))))
+                             (recur (switchV2 g (first c) (first i)) (rest i) (rest c)))))))))
 
 (comment
   (map vec (partition 2 (map vec (partition 2 (map parse-long (re-seq #"\d+" (slurp "input.txt") ) ) ) ) ) )
   (re-seq #"turn\soff|turn\son|toggle" (slurp "input.txt") )
   (reduce + (flatten (vec (repeat 10 (vec (repeat 10 0)))) ) )
+  (map #(if (> % 1) (dec %) 0) [0 1 2 3])
   (-main)
 
   )
